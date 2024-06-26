ee047f669099ff00575519a8793d3c4c7ecb55e3
Mary Stiphen Artienda <artienda.marystiphen@gmail.com>
2024-06-26 17:26:21 +0800

    sucess

---
+++ javascript/js/scripts.js
@@ -0,0 +1,14 @@
+function inner() {
+    document.getElementById("sample").innerHTML = "This is innerHTML";
+}
+
+function displayAlert() {
+   alert("This is an example alert");
+}
+function docWrite() {
+   document.write("This is an example of document.write");
+}
+function displayConsole() {
+    console.log("This is a console message");
+}
+
